63f3199ececae48911f612a96b73cb61a03e8c6f
Mariano Alfonso <mar.fer.alf@gmail.com>
2018-11-12 23:47:42 -0300

    Update de clases

---
+++ ConsoleApp1/ConsoleApp1/Clases/Novedad.cs
@@ -0,0 +1,70 @@
+﻿using ConsoleApp1.Conexion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Clases
+{
+    class Novedad : Insertable
+    {
+
+        private int idNovedad;
+        private int idUsuario;
+        private DateTime Fecha;
+        private int idMateria;
+        private string descripcion;
+
+
+        //CONSTRUCTOR 
+
+        public Novedad(int idNovedad, int idUsuario, DateTime fecha, int idMateria, string descripcion)
+        {
+            this.GetSetIdNovedad = idNovedad;
+            this.GetSetIdUsuario = idUsuario;
+            this.GetSetFecha = fecha;
+            this.GetSetIdMateria = idMateria;
+            this.GetSetDescripcion = descripcion;
+        }
+
+        //METODOS
+
+        public string getOrderedFields()
+        {
+            return "idNovedad, idusuario, fecha, idmateria, descripcion";
+        }
+
+        public string getOrderedValues()
+        {
+            string escape = "'";
+
+            return this.GetSetIdNovedad + ", " +
+                escape + this.GetSetIdUsuario + escape + ", " +
+                escape + this.GetSetFecha + escape + ", " +
+                escape + this.GetSetIdMateria + escape + ", " +
+                escape + this.GetSetDescripcion + escape;
+        }
+
+        public string getIdField()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getIdValue()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // GET Y SET
+
+        public int GetSetIdNovedad { get => idNovedad; set => idNovedad = value; }
+        public int GetSetIdUsuario { get => idUsuario; set => idUsuario = value; }
+        public DateTime GetSetFecha { get => Fecha; set => Fecha = value; }
+        public int GetSetIdMateria { get => idMateria; set => idMateria = value; }
+        public string GetSetDescripcion { get => descripcion; set => descripcion = value; }
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Clases/Usuario.cs
@@ -0,0 +1,89 @@
+﻿using ConsoleApp1.Conexion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Clases
+{
+    class Usuarios : Insertable
+    {
+        private int id_TipoUsuario;
+        private string nombre;
+        private string apellido;
+        private string dni;
+        private string usuario;
+        private string pass;
+        private string email;
+
+
+        //CONSTRUCTOR
+        public Usuarios(int idusuarios, int id_TipoUsuario, string nombre, string apellido, string dni, string usuario, string pass, string email)
+        {
+            this.GetSetIdusuarios = idusuarios;
+            this.GetSetId_TipoUsuario = id_TipoUsuario;
+            this.GetSetNombre = nombre;
+            this.GetSetApellido = apellido;
+            this.GetSetDni = dni;
+            this.GetSetUsuario = usuario;
+            this.GetSetPass = pass;
+            this.GetSetEmail = email;
+        }
+
+        public Usuarios(int id_TipoUsuario, string nombre, string apellido, string dni, string usuario, string pass, string email)
+        {
+            this.GetSetId_TipoUsuario = id_TipoUsuario;
+            this.GetSetNombre = nombre;
+            this.GetSetApellido = apellido;
+            this.GetSetDni = dni;
+            this.GetSetUsuario = usuario;
+            this.GetSetPass = pass;
+            this.GetSetEmail = email;
+        }
+
+        private Usuarios() { }
+
+        //METODOS
+
+        public string getOrderedFields()
+        {
+            return "id_tipo_usuario, nombre, apellido, dni, usuario, pass, email";
+        }
+
+        public string getOrderedValues()
+        {
+            string escape = "'";
+
+            return this.GetSetId_TipoUsuario+", "+
+                escape+this.GetSetNombre+escape+", " +
+                escape+this.GetSetApellido + escape + ", " +
+                escape+this.GetSetDni + escape + ", " +
+                escape+this.GetSetUsuario + escape + ", " +
+                escape+this.GetSetPass +escape + ", " +
+                escape+this.GetSetEmail +escape;
+        }
+
+        public string getIdField()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getIdValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        //GET Y SET
+
+        public int GetSetIdusuarios { get; set; }
+
+        public int GetSetId_TipoUsuario { get => id_TipoUsuario; set => id_TipoUsuario = value; }
+        public string GetSetNombre { get => nombre; set => nombre = value; }
+        public string GetSetApellido { get => apellido; set => apellido = value; }
+        public string GetSetDni { get => dni; set => dni = value; }
+        public string GetSetUsuario { get => usuario; set => usuario = value; }
+        public string GetSetPass { get => pass; set => pass = value; }
+        public string GetSetEmail { get => email; set => email = value; }
+    }
+}
